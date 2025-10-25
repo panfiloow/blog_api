8908a30ed73d760d70ba69acce8f1eaa39273e6d
DESKTOP-4CLG9BV\llya Panfilov <panfiloow@mail.ru>
2025-10-25 23:39:25 +0300

    Добавлен pgAdmin

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # Database
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+
+    # PgAdmin
+    PGADMIN_EMAIL: str
+    PGADMIN_PASSWORD: str
+    PGADMIN_PORT: int
 
     @property
     def DATABASE_URL_asyncpg(self):
